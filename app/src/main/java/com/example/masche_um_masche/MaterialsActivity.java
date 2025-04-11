ebbf0807223d36657140b1e5f576aeb8a3b56428
Kiyomi Wilberg <kiyomi.wilberg@adesso.de>
2025-04-11 17:35:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/masche_um_masche/MaterialsActivity.java
@@ -0,0 +1,14 @@
+package com.example.masche_um_masche;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MaterialsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_materials);
+    }
+}
---
+++ app/src/main/java/com/example/masche_um_masche/ProjectsActivity.java
@@ -0,0 +1,13 @@
+package com.example.masche_um_masche;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProjectsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_projects);
+    }
+}
